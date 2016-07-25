45f15d207105e9a7c80bbe6de1bbbbac373bb6b2
camilapretiz <camilapretiz@gmail.com>
2016-07-25 15:08:22 -0700

    soijfe

--- main.py
+++ main.py
@@ -9,26 +9,28 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_environment = jinja2.Environment(
   loader=jinja2.FileSystemLoader(template_dir))
 
-# class User(ndb.Model):
-#     name = ndb.StringProperty()
-#     music =
-#     food =
-#     personality =
-#     driver = ndb.
+class User(ndb.Model):
+    name = ndb.StringProperty()
+    music = ndb.StringProperty()
+    food = ndb.StringProperty()
+    personality = ndb.StringProperty()
+    driver = ndb.BooleanProperty()
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
         if user:
-            nickname = user.nickname()
-            logout_url = users.create_logout_url('/')
-            greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format(
-                nickname, logout_url)
+            self.redirect('/mainpage')
+            # nickname = user.nickname()
+            # logout_url = users.create_logout_url('/')
+            # greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format(
+            #     nickname, logout_url)
         else:
             login_url = users.create_login_url('/')
             greeting = '<a href="{}">Sign in</a>'.format(login_url)
 
-        self.response.write(
-            '<html><body>{}</body></html>'.format(greeting))
+        # self.response.write(
+        #     '<html><body>{}</body></html>'.format(greeting))
         template = jinja_environment.get_template('home.html')
         self.response.write(template.render())
 
