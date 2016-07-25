da97d990c421822998a45742b5b8581656435ce5
camilapretiz <camilapretiz@gmail.com>
2016-07-25 15:27:50 -0700

    login stuff ready

--- main.py
+++ main.py
@@ -20,28 +20,14 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
         if user:
-            nickname = user.nickname()
-            logout_url = users.create_logout_url('/')
-            greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format(
-                nickname, logout_url)
-        else:
-            login_url = users.create_login_url('/')
-            greeting = '<a href="{}">Sign in</a>'.format(login_url)
-
-        self.response.write(
-            '<html><body>{}</body></html>'.format(greeting))
-            
-        user = users.get_current_user()
-        if user:
             self.redirect('/mainpage')
 
-        else:
-            login_url = users.create_login_url('/')
-            greeting = '<a href="{}">Sign in</a>'.format(login_url)
-
+        # This creates the login link
+        login_url = users.create_login_url('/')
 
         template = jinja_environment.get_template('home.html')
-        self.response.write(template.render())
+        vals = {'login_url': login_url}
+        self.response.write(template.render(vals))
 
 class CreateAccountHandler(webapp2.RequestHandler):
     def get(self):
