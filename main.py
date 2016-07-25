41bcf353eae3a4d0b2a42479843d1605f8084342
camilapretiz <camilapretiz@gmail.com>
2016-07-25 11:40:11 -0700

    main.py stufff

--- main.py
+++ main.py
@@ -8,15 +8,58 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_environment = jinja2.Environment(
   loader=jinja2.FileSystemLoader(template_dir))
 
-
-
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template('home.html')
         self.response.write(template.render())
 
+class CreateAccountHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('newacc.html')
+        self.response.write(template.render())
+
+class UserInfoHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('userinfo.html')
+        self.response.write(template.render())
+
+class MainPageHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('mainpage.html')
+        self.response.write(template.render())
+
+class CreateTripHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('newtrip.html')
+        self.response.write(template.render())
+
+class TripInfoHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('tripinfo.html')
+        self.response.write(template.render())
+
+class JoinTripHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('jointrip.html')
+        self.response.write(template.render())
+
+class CreateAccountHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('newacc.html')
+        self.response.write(template.render())
+
+class EditTripHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('editrip.html')
+        self.response.write(template.render())
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    ('/
+    ('/newaccount', CreateAccountHandler),
+    ('/userinfo', UserInfoHandler),
+    ('/mainpage', MainPageHandler),
+    ('/newtrip', CreateTripHandler),
+    ('/tripinfo', TripInfoHandler),
+    ('/jointrip', JoinTripHandler),
+    ('/edittrip', EditTripHandler)
 ], debug=True)
